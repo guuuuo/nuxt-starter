8edd21ba62eb6fa8e006473be146258156b08c1d
Niko Guo <niko.guo@ehealth.com>
2025-10-23 20:33:25 +0800

    feat: Add Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+    css: ['~/assets/css/main.css'],
   devtools: { enabled: false },
-  modules: [],
+  modules: ['@nuxt/ui'],
 
   sourcemap: {
     client: 'hidden'
